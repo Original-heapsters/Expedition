ce1fbf5025736cd0d4c1507b0a574079074bcd25
Russell <sell_nat@yahoo.com>
2017-03-25 11:39:37 -0700

    Added basic work areas

--- app.py
+++ app.py
@@ -4,5 +4,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return "hello world"
+
+@app.route('/workarea1')
+def workarea1():
+    return "workarea1"
+
+
 if __name__ == "__main__":
     app.run(debug=True);
